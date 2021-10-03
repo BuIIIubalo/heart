38a969d33ebd564c0af1eac1abedba0c04e3eb52
Vitalik <58306595+BuIIIubalo@users.noreply.github.com>
2021-10-03 23:36:19 +0300

    Add files via upload

---
+++ add.js
@@ -0,0 +1,20 @@
+// Кроссбраузерные попытки вибрировать
+
+function vibrate(val){
+    if("vibrate" in navigator)  return navigator.vibrate(val);
+    if("oVibrate" in navigator)  return navigator.oVibrate(val);
+    if("mozVibrate" in navigator)  return navigator.mozVibrate(val);
+    if("webkitVibrate" in navigator)  return navigator.webkitVibrate(val);
+}
+
+function infiniteVibrate(val, interval) {
+    stopVibrate();
+    vInterval = setInterval(function() {
+        vibrate(val);
+    }, interval);
+}
+
+function stopVibrate() {
+    if(typeof(vInterval)!=="undefined") clearInterval(vInterval);
+    vibrate(0);
+}
